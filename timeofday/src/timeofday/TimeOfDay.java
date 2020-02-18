aa66acad805aaa3a24940e806fa62f336d732472
EliasDeCoppel <elias.decoppel@student.kuleuven.be>
2020-02-18 08:48:35 +0100

    finished project

---
+++ timeofday/src/timeofday/TimeOfDay.java
@@ -0,0 +1,22 @@
+package timeofday;
+
+public class TimeOfDay {
+	int minutesSinceMidnight;
+	
+	static int getHours(TimeOfDay t) {
+		return t.minutesSinceMidnight / 60;
+	}
+	
+	static int getMinutes(TimeOfDay t) {
+		return t.minutesSinceMidnight %= 60;
+	}
+	
+	static void setMinutes(TimeOfDay t,int value) {
+		t.minutesSinceMidnight = t.minutesSinceMidnight / 60 * 60 + value;
+	}
+	
+	static void setHours(TimeOfDay t, int value) {
+		t.minutesSinceMidnight %= 60;
+		t.minutesSinceMidnight += value * 60;
+	}
+}
